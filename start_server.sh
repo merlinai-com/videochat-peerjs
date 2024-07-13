02bbff6bb38cac7a822938eab3c0143dd053b4a5
Julian Darley <julian@merlin-ai.com>
2024-07-13 13:43:23 +0000

    install `pm2` and update `start_server.sh` to use it

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+## start_server.sh
 
-set -a; source .env; set +a
+## OLD VERSION
+# set -a; source .env; set +a
+# node server.js
 
-node server.js
+
+## Load nvm
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+## Install and use Node.js 20.2.0
+# nvm install 20.2.0
+# nvm use 20.2.0
+
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
+## Install PM2 if not installed
+# if ! command -v pm2 &> /dev/null
+# then
+#     npm install -g pm2
+# fi
+
+# Start the server with PM2
+pm2 start server.js --watch
