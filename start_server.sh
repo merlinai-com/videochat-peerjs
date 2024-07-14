9df45471536e0fa5ec1aacec7f246b941153bed3
Julian Darley <julian@merlin-ai.com>
2024-07-14 09:39:29 +0000

    update `start_server.sh`

--- start_server.sh
+++ start_server.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 ## start_server.sh
 
-## OLD VERSION
-# set -a; source .env; set +a
-# node server.js
-
-
 ## Load nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 ## Install and use Node.js 20.2.0
-# nvm install 20.2.0
+nvm install 20.2.0
 nvm use 20.2.0
 
 # Load environment variables from .env file
@@ -20,10 +15,10 @@ source .env
 set +a
 
 ## Install PM2 if not installed
-# if ! command -v pm2 &> /dev/null
-# then
-#     npm install -g pm2
-# fi
+if ! command -v pm2 &> /dev/null
+then
+    npm install -g pm2
+fi
 
 # Start the server with PM2
 ## pm2 start server.js --watch
