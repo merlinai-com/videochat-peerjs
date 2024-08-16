447e38865ffcfcb140cb20f50c80b653dfc6a431
Zoe Stafford <zoepolarsax@gmail.com>
2024-08-16 14:45:55 +0100

    Add string handling utilities

--- backend/lib/utils.ts
+++ backend/lib/utils.ts
@@ -21,3 +21,32 @@ export function enumerate<T>(vals: T[]): { index: number; value: T }[] {
 export function uniq<T>(vals: T[]): T[] {
     return [...new Set(vals)];
 }
+
+/** Split a string in to a list of words */
+export function words(value: string): string[] {
+    return value.split(/\s+/);
+}
+
+/**
+ * Convert a string or list of strings to kebab case
+ *
+ * Note: This ignores multiple consecutive spaces and empty words
+ */
+export function kebabCase(ws: string | string[]): string {
+    if (!Array.isArray(ws)) ws = words(ws);
+    else ws = ws.filter((w) => w);
+
+    return ws.join("-");
+}
+
+/**
+ * Convert a string or list of stirngs to snake case
+ *
+ * Note: This ignores multiple consecutive spaces and empty words
+ */
+export function snakeCase(ws: string | string[]): string {
+    if (!Array.isArray(ws)) ws = words(ws);
+    else ws = ws.filter((w) => w);
+
+    return ws.join("_");
+}
